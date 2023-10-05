b01259ad69d0393189969ef60261c12b4c049f7e
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2023-10-05 17:51:24 +0900

    added: #15 プロジェクト名変更

--- src/rdetoolkit/__main__.py
+++ src/rdetoolkit/__main__.py
@@ -35,6 +35,10 @@ def make_template_json(path: Path) -> None:
     with open(path, mode='w', encoding="utf-8") as f:
         json.dump({}, f, indent=4)
 
+def make__json(path: Path) -> None:
+    with open(path, mode='w', encoding="utf-8") as f:
+        json.dump({}, f, indent=4)
+
 
 @click.group()
 def cli():
@@ -45,32 +49,32 @@ def cli():
 def init() -> None:
     """Initialize the project."""
     directorys = [
-        Path('modules'),
-        Path('data/inputdata'),
-        Path('data/invoice'),
-        Path('data/tasksupport')
+        Path('container/modules'),
+        Path('container/data/inputdata'),
+        Path('container/data/invoice'),
+        Path('container/data/tasksupport')
     ]
     try:
         for dir in directorys:
             dir.mkdir(parents=True, exist_ok=True)
 
-        if not Path('main.py').exists():
-            make_main_py(Path('data/main.py'))
+        if not Path('container/main.py').exists():
+            make_main_py(Path('container/main.py'))
 
-        if not Path('data/requirements.txt').exists():
-            make_requirements_txt(Path('data/requirements.txt'))
+        if not Path('container/requirements.txt').exists():
+            make_requirements_txt(Path('container/requirements.txt'))
 
-        if not Path('data/invoice/invoice.json').exists():
-            make_template_json(Path('data/invoice/invoice.json'))
+        if not Path('container/data/invoice/invoice.json').exists():
+            make_template_json(Path('container/data/invoice/invoice.json'))
 
-        if not Path('data/tasksupport/invoice.schema.json').exists():
-            make_template_json(Path('data/tasksupport/invoice.schema.json'))
+        if not Path('container/data/tasksupport/invoice.schema.json').exists():
+            make_template_json(Path('container/data/tasksupport/invoice.schema.json'))
 
-        if not Path('data/tasksupport/meatadata-def.json').exists():
-            make_template_json(Path('data/tasksupport/metadata-def.json'))
+        if not Path('container/data/tasksupport/meatadata-def.json').exists():
+            make_template_json(Path('container/data/tasksupport/metadata-def.json'))
 
         click.echo(click.style("Ready to develop a structured program for RDE.", fg="green"))
-        click.echo("\nCheck the folder you created: data\n")
+        click.echo("\nCheck the folder you created: container\n")
         click.echo("Done!")
     except Exception:
         click.echo(click.style("Failed to create files required for structured RDE programs.", fg="red"), err=True)
