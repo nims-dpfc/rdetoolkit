605dc85f49f6aca7df6c9583b0d76c169741dc93
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2023-12-19 17:07:10 +0900

    deleted: #41 Stubの追加をCIで実行するため

--- stubs/rdetoolkit/rdelogger.pyi
+++
@@ -1,6 +0,0 @@
-import logging
-from rdetoolkit.rde2util import StorageDir as StorageDir
-from typing import Optional
-
-def get_logger(name: str, *, file_path: Optional[str] = ...) -> logging.Logger: ...
-def write_job_errorlog_file(code: int, message: str, *, filename: str = ...) -> None: ...
