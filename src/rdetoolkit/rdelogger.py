46c7b38114dc6c51c108a87ecb42dfcbacc704cb
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-04-23 17:19:03 +0900

    added: #76 ユーザーログ関数を実装

--- src/rdetoolkit/rdelogger.py
+++ src/rdetoolkit/rdelogger.py
@@ -1,8 +1,12 @@
 import logging
 import os
+import pprint
+from logging import DEBUG, INFO, FileHandler, Formatter, Logger, NullHandler, StreamHandler, getLogger
 from typing import Optional
 
 from rdetoolkit.rde2util import StorageDir
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def get_logger(name: str, *, file_path: Optional[str] = None) -> logging.Logger:
@@ -74,3 +78,103 @@ def write_job_errorlog_file(code: int, message: str, *, filename: str = "job.fai
     ) as f:
         f.write(f"ErrorCode={code}\n")
         f.write(f"ErrorMessage={message}\n")
+
+
+class CustomLog:
+    """The CustomLog class is a class for writing custom logs to a user's log file.
+
+    To create an instance of this class, call `CustomLog` with the module name as an argument.
+    Then, by calling the `get_log` method, you can get a logger for writing logs.
+
+    Returns:
+        logger: A logger instance for writing logs. If `False` is passed to `get_log`, the logger will not write any logs.
+
+    Example:
+    ```python
+    logger = CustomLog(__name__).get_log()
+
+    # If you do not want to write a log, pass `False` as an argument to the `get_log` method.
+    logger = CustomLog(__name__).get_log(False)
+
+    #In the above code, a logger is generated, but no log is written.
+    ```
+    """
+
+    def __init__(self, name: str = "rdeuser"):
+        logger = getLogger(name)
+        logger.propagate = False
+        logger.setLevel(DEBUG)
+
+        self.logger = logger
+
+    def get_logger(self, needLogs: bool = True) -> Logger:
+        """Retrieves the logger instance.
+
+        Args:
+            needLogs (bool, optional): Indicates whether logs are needed. Defaults to True.
+
+        Returns:
+            Logger: The logger instance.
+
+        """
+        logger = self.logger
+        if not logger.hasHandlers():
+            logDir = StorageDir.get_specific_outputdir(True, "logs")
+            logFile = logDir / "rdeuser.log"
+            if needLogs:
+                self._set_handler(StreamHandler(), True)
+                self._set_handler(FileHandler(logFile), True)
+            else:
+                self._set_handler(NullHandler(), False)
+        self.logger = logger
+
+        return self.logger
+
+    def _set_handler(self, handler, verbose: bool):
+        level = DEBUG if verbose else INFO
+        handler.setLevel(level)
+        formatter = Formatter(
+            # fmt="%(asctime)s - [%(name)s](%(levelname)s)" +
+            #       "%(funcName)-15s %(message)s",
+            fmt="%(asctime)s (%(levelname)s) %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S %z",
+        )
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+
+def log_decorator():
+    """A decorator function that logs the start and end of a decorated function.
+
+    Returns:
+        function: The decorated function.
+
+    Example:
+        ```python
+        @log_decorator()
+        def my_function():
+            print("Hello, World!")
+
+        my_function()
+        # Output:
+        # my_function     --> Start
+        # Hello, World!
+        # my_function     <-- End
+        ```
+    """
+
+    def _log_decorator(func):
+        def wrapper(*args, **kargs):
+            logger = CustomLog().get_logger()
+            logger.info(f"{func.__name__:15} --> Start")
+            try:
+                return func(*args, **kargs)
+            except Exception as e:
+                logger.error(f"{func.__name__:15} !!! Error has occurred")
+                raise e
+            finally:
+                logger.info(f"{func.__name__:15} <-- End")
+
+        return wrapper
+
+    return _log_decorator
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,9 +1,46 @@
 import logging
+import logging.handlers
 import os
 import pathlib
 import shutil
+from unittest import mock
 
-from rdetoolkit.rdelogger import get_logger
+from rdetoolkit.rdelogger import get_logger, CustomLog, log_decorator
+
+
+def test_custom_log():
+    custom_log = CustomLog('test')
+
+    logger = custom_log.get_logger()
+
+    # # Confirm that the logger was correctly obtained
+    assert logger is not None
+    # Confirm that the logger name is correct
+    assert logger.name == 'test'
+    # Confirm that the logger has a handler
+    assert len(logger.handlers) > 0
+
+
+def test_log_decorator(caplog):
+    logger_mock = mock.Mock(spec=CustomLog)
+    logger_mock.get_logger.return_value = mock.Mock()
+
+    # ロガーのモックをパッチ
+    with mock.patch('rdetoolkit.rdelogger.CustomLog', return_value=logger_mock):
+        @log_decorator()
+        def test_func():
+            return "Hello, World!"
+
+        result = test_func()
+
+        assert result == "Hello, World!"
+
+        # ロガーが正しく呼び出されたことを確認
+        calls = [
+            mock.call.info('test_func       --> Start'),
+            mock.call.info('test_func       <-- End')
+        ]
+        logger_mock.get_logger.return_value.assert_has_calls(calls)
 
 
 def test_get_logger_without_filepath(tmpdir):
