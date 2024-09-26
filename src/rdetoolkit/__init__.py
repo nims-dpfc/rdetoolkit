c42fe8d5ed190f2618cc2cc4ce19cda429704e71
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-09-26 18:26:03 +0900

    added: #2 resize_image_aspect_ratio function to rdetoolkit_core

--- src/rdetoolkit/__init__.py
+++ src/rdetoolkit/__init__.py
@@ -4,3 +4,4 @@ from . import exceptions, invoicefile, modeproc, rde2util, rdelogger, workflows
 from .impl import *
 from .interfaces import *
 from .models import *
+from .rdetoolkit_core import resize_image_aspect_ratio
---
+++ src/rdetoolkit/rdetoolkit_core.pyi
@@ -0,0 +1,4 @@
+from .rdetoolkit_core import resize_image_aspect_ratio
+
+def resize_image_aspect_ratio(input_path: str, output_path: str, width: int, height: int) -> None:
+    ...
