e72510932fd5ff585e9b86883895028f54cde6c4
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-09-27 15:19:39 +0900

    changed: #45 change the location of stub files

--- src/rdetoolkit/rdetoolkit_core.pyi
+++ src/rdetoolkit/rdetoolkit_core.pyi
@@ -1,4 +1,13 @@
-from .rdetoolkit_core import resize_image_aspect_ratio
+from rdetoolkit_core import resize_image_aspect_ratio
 
 def resize_image_aspect_ratio(input_path: str, output_path: str, width: int, height: int) -> None:
+    """Resize the image while maintaining the aspect ratio.
+
+    Args:
+        input_path (str): The path of the image file to be resized.
+        output_path (str): The path of the resized image file.
+        width (int): The width of the resized image.
+        height (int): The height of the resized image.
+
+    """
     ...
