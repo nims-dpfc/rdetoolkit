0334be53177d3569173059ceff2a108df7317a9c
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-09-30 07:22:53 +0900

    changed: #2 rename rdetoolkit_core to rdetoolkit-core in lib.rs

--- rdetoolkit-core/lib.rs
+++ rdetoolkit-core/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 mod imageutil;
 
 #[pymodule]
-pub fn rdetoolkit_core(module: &Bound<'_, PyModule>) -> PyResult<()> {
+pub fn core(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(
         imageutil::processing::resize_image_aspect_ratio,
         module
