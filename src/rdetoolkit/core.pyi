3fd5360c376ef59d07179fde60ce92a938148d21
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-10-01 17:07:13 +0900

    changed: #2 image resizing function to remove circular import

--- src/rdetoolkit/core.pyi
+++ src/rdetoolkit/core.pyi
@@ -1,5 +1,3 @@
-from .core import resize_image_aspect_ratio
-
 def resize_image_aspect_ratio(input_path: str, output_path: str, width: int, height: int) -> None:
     """Resize the image while maintaining the aspect ratio.
 
