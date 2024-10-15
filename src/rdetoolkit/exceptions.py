2434f5793f3329870e7f775ee9132db1a95da98b
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-10-15 19:08:56 +0900

    Refactor: #46 exception classes for invoice modes and multi-data tile mode

--- src/rdetoolkit/exceptions.py
+++ src/rdetoolkit/exceptions.py
@@ -28,6 +28,99 @@ class StructuredError(Exception):
         self.traceback_info = traceback_info
 
 
+class InvoiceModeError(Exception):
+    """Exception raised for errors related to invoice mode.
+
+    Attributes:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 100.
+        eobj (Any | None): Optional object related to the exception. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+
+    Args:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 100.
+        eobj (Any | None): Optional object related to the exception. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+    """
+    def __init__(self, emsg: str = "", ecode: int = 100, eobj: Any | None = None, traceback_info: str | None = None) -> None:
+        super().__init__(emsg)
+        self.emsg = emsg
+        self.ecode = ecode
+        self.eobj = eobj
+        self.traceback_info = traceback_info
+
+
+class ExcelInvoiceModeError(Exception):
+    """Exception raised for errors related to Excelinvoice mode.
+
+    Attributes:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 101.
+        eobj (Any | None): Optional object related to the exception. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+
+    Args:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 102.
+        eobj (Any | None): Optional object related to the exception. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+    """
+    def __init__(self, emsg: str = "", ecode: int = 101, eobj: Any | None = None, traceback_info: str | None = None) -> None:
+        super().__init__(emsg)
+        self.emsg = emsg
+        self.ecode = ecode
+        self.eobj = eobj
+        self.traceback_info = traceback_info
+
+
+class MultiDataTileModeError(Exception):
+    """Exception raised for errors in MultiData tile mode operations.
+
+    Attributes:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 102.
+        eobj (Any | None): Optional object related to the error. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+
+    Args:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 101.
+        eobj (Any | None): Optional object related to the error. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+    """
+
+    def __init__(self, emsg: str = "", ecode: int = 102, eobj: Any | None = None, traceback_info: str | None = None) -> None:
+        super().__init__(emsg)
+        self.emsg = emsg
+        self.ecode = ecode
+        self.eobj = eobj
+        self.traceback_info = traceback_info
+
+
+class RdeFormatModeError(Exception):
+    """Exception raised for errors in the RDE format mode.
+
+    Attributes:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 103.
+        eobj (Any | None): Optional object related to the error. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+
+    Args:
+        emsg (str): Error message describing the exception.
+        ecode (int): Error code associated with the exception. Default is 103.
+        eobj (Any | None): Optional object related to the error. Default is None.
+        traceback_info (str | None): Optional traceback information. Default is None.
+    """
+    def __init__(self, emsg: str = "", ecode: int = 103, eobj: Any | None = None, traceback_info: str | None = None) -> None:
+        super().__init__(emsg)
+        self.emsg = emsg
+        self.ecode = ecode
+        self.eobj = eobj
+        self.traceback_info = traceback_info
+
+
 class InvoiceSchemaValidationError(Exception):
     """Raised when a validation error occurs."""
 
--- src/rdetoolkit/exceptions.pyi
+++ src/rdetoolkit/exceptions.pyi
@@ -1,5 +1,5 @@
 import traceback
-from _typeshed import Incomplete as Incomplete
+from _typeshed import Incomplete
 from typing import Any, Callable
 
 class StructuredError(Exception):
@@ -8,6 +8,34 @@ class StructuredError(Exception):
     eobj: Incomplete
     traceback_info: Incomplete
     def __init__(self, emsg: str = '', ecode: int = 1, eobj: Any | None = None, traceback_info: str | None = None) -> None: ...
+
+class InvoiceModeError(Exception):
+    emsg: Incomplete
+    ecode: Incomplete
+    eobj: Incomplete
+    traceback_info: Incomplete
+    def __init__(self, emsg: str = '', ecode: int = 100, eobj: Any | None = None, traceback_info: str | None = None) -> None: ...
+
+class ExcelInvoiceModeError(Exception):
+    emsg: Incomplete
+    ecode: Incomplete
+    eobj: Incomplete
+    traceback_info: Incomplete
+    def __init__(self, emsg: str = '', ecode: int = 101, eobj: Any | None = None, traceback_info: str | None = None) -> None: ...
+
+class MultiDataTileModeError(Exception):
+    emsg: Incomplete
+    ecode: Incomplete
+    eobj: Incomplete
+    traceback_info: Incomplete
+    def __init__(self, emsg: str = '', ecode: int = 102, eobj: Any | None = None, traceback_info: str | None = None) -> None: ...
+
+class RdeFormatModeError(Exception):
+    emsg: Incomplete
+    ecode: Incomplete
+    eobj: Incomplete
+    traceback_info: Incomplete
+    def __init__(self, emsg: str = '', ecode: int = 103, eobj: Any | None = None, traceback_info: str | None = None) -> None: ...
 
 class InvoiceSchemaValidationError(Exception):
     message: Incomplete
