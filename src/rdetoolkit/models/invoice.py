1bbd8df5e4ad6ab3f49adce726fd96d5fe012594
Sonokawa Hayato <SONOKAWA.Hayato@nims.go.jp>
2024-12-27 09:46:15 +0900

    feat: #13 Add models for ExcelInvoice template generation and implement conversion to DataFrame templates
    - Implement data classes and models to manage the default configuration of ExcelInvoice.
    - Add a class to search for target values from `SpecificTerm` and `GeneralTerm` CSV files.
    - Add configuration classes for template file settings.

---
+++ src/rdetoolkit/models/invoice.py
@@ -0,0 +1,225 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Literal
+
+import polars as pl
+from openpyxl.utils import get_column_letter
+from pydantic import BaseModel, Field
+
+from rdetoolkit.exceptions import DataRetrievalError, InvalidSearchParametersError
+
+
+class HeaderRow1(BaseModel):
+    A1: str = "invoiceList_format_id"
+
+
+class HeaderRow2(BaseModel):
+    A2: str = "data_file_names"
+    D2_G2: list[str] = Field(default_factory=lambda: ["basic"] * 4)
+    H2_M2: list[str] = Field(default_factory=lambda: ["sample"] * 6)
+
+
+class HeaderRow3(BaseModel):
+    A3: str = "name"
+    B3: str = "dataset_title"
+    C3: str = "dataOwner"
+    D3: str = "dataOwnerId"
+    E3: str = "dataName"
+    F3: str = "experimentId"
+    G3: str = "description"
+    H3: str = "names"
+    I3: str = "sampleId"
+    J3: str = "ownerId"
+    K3: str = "composition"
+    L3: str = "referenceUrl"
+    M3: str = "description"
+
+
+class HeaderRow4(BaseModel):
+    A4: str = "ファイル名\n(拡張子も含め入力)\n(入力例:○○.txt)"
+    B4: str = "データセット名\n(必須)"
+    C4: str = "データ所有者\n(NIMS User ID)"
+    D4: str = "NIMS user UUID\n(必須)"
+    E4: str = "データ名\n(必須)"
+    F4: str = "実験ID"
+    G4: str = "説明"
+    H4: str = "試料名\n(ローカルID)"
+    I4: str = "試料UUID\n(必須)"
+    J4: str = "試料管理者UUID"
+    K4: str = "化学式・組成式・分子式など"
+    L4: str = "参考URL"
+    M4: str = "試料の説明"
+
+
+class FixedHeaders(BaseModel):
+    row1: HeaderRow1 = HeaderRow1()
+    row2: HeaderRow2 = HeaderRow2()
+    row3: HeaderRow3 = HeaderRow3()
+    row4: HeaderRow4 = HeaderRow4()
+
+    def to_template_dataframe(self) -> pl.DataFrame:
+        """Converts the invoice data to a Polars DataFrame formatted for a template.
+
+        This method organizes the invoice data into a DataFrame with specific padding
+        and column structure suitable for template processing.
+
+        Returns:
+            pl.DataFrame: A Polars DataFrame containing the formatted invoice data.
+        """
+        padding_value = None
+        row1 = [self.row1.A1] + [padding_value] * 12
+        row2 = [self.row2.A2] + [padding_value] * 2 + self.row2.D2_G2 + self.row2.H2_M2 + [padding_value]
+        row3 = list(self.row3.model_dump().values())
+        row4 = list(self.row4.model_dump().values())
+
+        data = [row1, row2, row3, row4]
+        columns = [get_column_letter(i) for i in range(1, 14)]
+
+        return pl.DataFrame(data, columns)
+
+
+@dataclass
+class TemplateConfig:
+    schema_path: str | Path
+    general_term_path: str | Path
+    specific_term_path: str | Path
+    inputfile_mode: Literal["file", "folder"] = "file"
+
+
+class GeneralTermRegistry:
+
+    def __init__(self, csv_path: str):
+        self.df = pl.read_csv(csv_path)
+
+    def search(self, column: str, value: str, out_cols: list[str]) -> list[dict[str, Any]]:
+        """Search for rows in the DataFrame where the specified column matches the given value and return selected columns.
+
+        Args:
+            column (str): The name of the column to search.
+            value (str): The value to search for in the specified column.
+            out_cols (list[str]): A list of column names to include in the output.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries representing the rows that match the search criteria, with only the specified columns included.
+        """
+        filtered_df = self.df.filter(pl.col(column) == value)
+        return filtered_df.select(out_cols).to_dicts()
+
+    def by_term_id(self, term_id: str) -> list[dict[str, Any]]:
+        """Retrieve a list of dictionaries containing invoice details filtered by term ID.
+
+        Args:
+            term_id (str): The term ID to filter invoices by.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries, each containing the keys
+            "term_id", "key_name", and "term_description" with their corresponding values.
+        """
+        return self.search("term_id", term_id, ["term_id", "key_name", "ja", "en"])
+
+    def by_ja(self, ja_text: str) -> list[dict[str, Any]]:
+        """Search for records with Japanese text.
+
+        Args:
+            ja_text (str): The Japanese text to search for.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries containing the search results with keys "term_id", "key_name", and "term_description".
+        """
+        return self.search("ja", ja_text, ["term_id", "key_name", "en"])
+
+    def by_en(self, en_text: str) -> list[dict[str, Any]]:
+        """Search for terms in English text.
+
+        Args:
+            en_text (str): The English text to search for.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries containing the search results with keys "term_id", "key_name", and "term_description".
+        """
+        return self.search("en", en_text, ["term_id", "key_name", "ja"])
+
+
+class SpecificTermRegistry:
+
+    def __init__(self, csv_path: str):
+        self.df = pl.read_csv(csv_path)
+
+    def search(self, columns: list[str], values: list[str], out_cols: list[str]) -> list[dict[str, Any]]:
+        """Search for rows in the DataFrame where the specified column matches the given value and return the selected columns.
+
+        Args:
+            columns (list[str]): The name of the column to search.
+            values (list[str]): The value to search for in the specified column.
+            out_cols (list[str]): A list of column names to include in the output.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries representing the rows that match the search criteria, with only the specified columns included.
+        """
+        if len(columns) != len(values):
+            emsg = "The lengths of 'columns' and 'values' must be the same."
+            raise ValueError(emsg)
+
+        try:
+            filter_expr = pl.lit(True)
+            for col, val in zip(columns, values):
+                expr = pl.col(col) == val
+                filter_expr = expr if filter_expr is None else filter_expr & expr
+            filtered_df = self.df.filter(filter_expr)
+
+            if filtered_df.is_empty():
+                terms = ",".join(f"{col}={val}" for col, val in zip(columns, values))
+                emsg = f"No results found for the specified terms: {terms}."
+                raise InvalidSearchParametersError(emsg)
+            return filtered_df.select(out_cols).to_dicts()
+        except Exception as e:
+            terms = ",".join(f"{col}={val}" for col, val in zip(columns, values))
+            emsg = f"An error occurred while searching for terms: {terms}. Error: {e}"
+            raise DataRetrievalError(emsg) from e
+
+    def by_term_and_class_id(self, term_id: str, sample_class_id: str) -> list[dict[str, Any]]:
+        """Retrieve a list of dictionaries containing specific fields for entries matching the given term and sample class IDs.
+
+        Args:
+            term_id (str): The term ID to search for.
+            sample_class_id (str): The sample class ID to search for.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries, each containing the fields "sample_class_id", "terma_id", "key_name", "ja", and "en" for entries that match the term and sample class IDs.
+        """
+        return self.search(["sample_class_id", "terma_id"], [term_id, sample_class_id], ["sample_class_id", "terma_id", "key_name", "ja", "en"])
+
+    def by_key_name(self, key_name: list[str]) -> list[dict[str, Any]]:
+        """Retrieve a list of dictionaries containing specific fields for entries matching the given key name.
+
+        Args:
+            key_name (list[str]): The key name to search for.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries, each containing the fields "sample_class_id", "terma_id", "key_name", "ja", and "en" for entries that match the key name.
+        """
+        return self.search(["key_name"], key_name, ["sample_class_id", "terma_id", "key_name", "ja", "en"])
+
+    def by_ja(self, ja_text: list[str]) -> list[dict[str, Any]]:
+        """Search for records by Japanese text.
+
+        Args:
+            ja_text (list[str]): The Japanese text to search for.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries containing the search results.
+        """
+        return self.search(["ja"], ja_text, ["sample_class_id", "term_id", "key_name", "en"])
+
+    def by_en(self, en_text: list[str]) -> list[dict[str, Any]]:
+        """Search for records in the database using the provided English text.
+
+        Args:
+            en_text (list[str]): The English text to search for.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries containing the search results.
+        """
+        return self.search(["en"], en_text, ["sample_class_id", "term_id", "key_name", "ja"])
